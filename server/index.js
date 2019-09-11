2a0cfae6dcbc5879c241de94ef027f77ae17e700
Sonia <soniamianji1@gmail.com>
2019-09-11 14:49:03 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+const logger = require("morgan");
+const config = require("./config");
+
+console.log("hello");
