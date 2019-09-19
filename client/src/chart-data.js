f1569c8c224a09b32a7fc5e690a14d79d5e0adc4
marcusjohnsson <mr.marcus.marega@gmail.com>
2019-09-19 17:05:11 +0200

    chart

--- client/src/chart-data.js
+++ client/src/chart-data.js
@@ -1,91 +1,11 @@
-// Build the script here.
+// Recieved object of oberservations has the following criteria:
+// 1) must only belong to 1 compound.
+// 2) must belong to a completed survey (survey completed != null)
 
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-// START of HELPER FUNCTIONS
+// # 1 - Create an array of the effectNames (no dublicates)
+// # 2 - Pipe each elemt of that array through the effects compiler, returns a an array of objects.
 
-//// Search an object ie: getObjects(observations, 'effect_name', 'headache');
-/*
-    * @param (obj{}, "val", "val")
-    /* returns obj{}
-    */
-function getObjects(obj, key, val) {
-	var objects = [];
-	for (var i in obj) {
-		if (!obj.hasOwnProperty(i)) continue;
-		if (typeof obj[i] == 'object') {
-			objects = objects.concat(getObjects(obj[i], key, val));
-		} else if (i == key && obj[key] == val) {
-			objects.push(obj);
-		}
-	}
-	return objects;
-}
-
-//// Removes dublicate intigers from array
-/*
-    * @param (array[])
-    /* returns array[]
-    */
-function removeDuplicates(num) {
-	var x,
-		len = num.length,
-		out = [],
-		obj = {};
-
-	for (x = 0; x < len; x++) {
-		obj[num[x]] = 0;
-	}
-	for (x in obj) {
-		out.push(x);
-	}
-	return out;
-}
-
-//// Function to test if a value t is within range x1 and x2.
-/*
-    /* @param (int, int, int)
-    /* returns TRUE / FALSE
-    */
-function isBetween(timestamp, x1, x2) {
-	return timestamp >= x1 && timestamp <= x2;
-}
-//console.log('That 4 belongs to [1,3] is', isBetween(4, 1, 3));
-
-//// Funtion that calculate the average of an array if intergers
-/*
-    /* @param ([int,int])
-    /* returns int
-    */
-function average(array) {
-	// Removes NaN
-	var filteredArray = array.filter(function(el) {
-		return el != null;
-	});
-	// Averages the array using lowdash: https://lodash.com/docs/4.17.15#mean
-	return _.mean(filteredArray);
-}
-//var nums = [ null, null, 3 ];
-//console.log('Averages [null, null, 3]', average(nums));
-
-//// Funtion that translates UNIX timestamps to HH:MM:SS
-/* @param unixtimestamp
-    /* returns var
-    */
-function hhmmss(unixtimestamp) {
-	var time = new Date(unixtimestamp * 1000);
-	var hours = time.getHours() - 1; /// NOTE (-1) bug fix
-	var minutes = '0' + time.getMinutes();
-	var seconds = '0' + time.getSeconds();
-
-	// Will display time in 10:30:23 format
-	var hhmmss = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-	return hhmmss;
-}
-
-// END of HELPER FUNCTIONS
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
+///////////////////////////
 
 var computation = {
 	type: 'line',
