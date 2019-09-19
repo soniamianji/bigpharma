d185d71dd498e1ec1a07dbda29eac93668cf6a38
marcusjohnsson <mr.marcus.marega@gmail.com>
2019-09-19 09:25:24 +0200

    somethingı

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,43 @@
 <template>
+  <!-- App.vue -->
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Big Pharma</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn>
-        <router-link to="/login" class="white--text">login</router-link>
-      </v-btn>
-      <v-btn>
-        <router-link to="/" class="white--text">Home</router-link>
-      </v-btn>
+    <v-app-bar app>
+      <!-- -->
     </v-app-bar>
-
-    <v-content></v-content>
-    <!-- <v-card height="150">
-      <v-footer absolute class="font-weight-medium">
-        <v-col class="text-center" cols="12">
-          {{ new Date().getFullYear() }} —
-          <strong>Big Pharma</strong>
-        </v-col>
-      </v-footer>
-    </v-card>-->
-    <router-view />
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <v-parallax
+        dark
+        src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1687,w_3000,x_0,y_0/dpr_1.5/c_limit,w_908/fl_lossy,q_auto/v1556161625/190425-clift-congress-big-pharma-hero_gdaeyp"
+      >
+        <v-row align="center" justify="center">
+          <h1 class="display-2 font-weight-thin mb-4">BIGPHARMA</h1>
+          <h4 class="subheading">Clinical trials Informatics</h4>
+        </v-row>
+      </v-parallax>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <v-item-group>
+          <v-container>
+            <v-row>
+              <v-col v-for="n in 12" :key="n" cols="12" md="4">
+                <v-item v-slot:default="{ active, toggle }">
+                  <v-card
+                    :color="active ? 'primary' : ''"
+                    class="d-flex align-center"
+                    dark
+                    height="200"
+                    @click="toggle"
+                  >
+                    <v-scroll-y-transition>
+                      <div v-if="active" class="display-3 flex-grow-4 text-center">Active</div>
+                    </v-scroll-y-transition>
+                  </v-card>
+                </v-item>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-item-group>
+      </v-container>
+    </v-content>
   </v-app>
-</template>
-
-
-
--- client/src/router.js
+++ client/src/router.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
-import Login from "./views/Login.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/login",
-      name: "Login",
-      component: Login
-    }
-  ]
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: Home
+		},
+		{
+			path: '/login',
+			name: 'Login',
+			component: Login
+		}
+	]
 });
