6d0dca04f78d35ef5c5e0b82e08d93b200bfe3b4
marcusjohnsson <mr.marcus.marega@gmail.com>
2019-09-20 12:15:42 +0200

    Observations Mocks (non componented)

--- client/src/views/Observations.vue
+++ client/src/views/Observations.vue
@@ -5,15 +5,22 @@
     <v-container fluid class="mt-12">
       <!--START of INPUTS -->
       <v-card max-width="600" class="mx-auto mt-10">
-        <div class="text-center">
-          <v-btn text small>
-            <v-icon left>mdi-clock</v-icon>00:00:00
-          </v-btn>
-        </div>
+        <v-menu bottom left>
+          <template v-slot:activator="{ on }">
+            <v-btn text small v-on="on">
+              <v-icon left>mdi-clock</v-icon>
+              {{time}}
+            </v-btn>
+          </template>
+          <v-time-picker v-model="time" format="24hr" use-seconds></v-time-picker>
+        </v-menu>
+        <v-btn text small disabled>Edit</v-btn>
+        <v-btn text small disabled>Delete</v-btn>
+
         <v-divider></v-divider>
         <v-card class="d-flex justify-space-between" flat tile>
           <v-card-text>
-            <v-select :items="items" label="Choose an effect"></v-select>
+            <v-select :items="items" label="Effect"></v-select>
           </v-card-text>
         </v-card>
         <v-card class="d-flex justify-space-between mt-n8" flat tile>
@@ -33,11 +40,56 @@
       </v-card>
     </v-container>
     <!--END OF INPUTS -->
-    <v-btn text color>add new entry</v-btn>
+    <v-card max-width="300" class="mx-auto mt-5 mb-10">
+      <v-btn width="100%" text color>New Observation</v-btn>
+    </v-card>
     <v-divider></v-divider>
     <!--END OF FORM -->
-    <!--START OF TIMELINE -->
-    <v-container fluid class="mt-0" style="max-width: 600px;"></v-container>
+    <!----------------------------->
+    <!--START OF FORM -->
+    <div class="grey lighten-2">
+      <!--START of INPUTS -->
+      <v-container>
+        <v-card flat max-width="600" class="mx-auto mt-10">
+          <v-menu bottom left>
+            <template v-slot:activator="{ on }">
+              <v-btn disabled text small v-on="on">
+                <v-icon left>mdi-clock</v-icon>
+                {{time}}
+              </v-btn>
+            </template>
+            <v-time-picker v-model="time" format="24hr" use-seconds></v-time-picker>
+          </v-menu>
+          <v-btn text small>Edit</v-btn>
+          <v-btn text small>Delete</v-btn>
+
+          <v-divider></v-divider>
+          <v-card class="d-flex justify-space-between" flat tile>
+            <v-card-text>
+              <v-select disabled :items="items" label="Headache"></v-select>
+            </v-card-text>
+          </v-card>
+          <v-card class="d-flex justify-space-between mt-n8" flat tile>
+            <v-card-text>
+              <v-slider
+                disabled
+                class="pa-2"
+                v-model="fruits"
+                :tick-labels="ticksLabels"
+                :max="10"
+                step="1"
+                ticks="always"
+                tick-size="4"
+                label="Intensity"
+              ></v-slider>
+            </v-card-text>
+          </v-card>
+        </v-card>
+      </v-container>
+      <v-card max-width="175" class="mx-auto mt-2 mb-10">
+        <v-btn width="100%" text color>Finalize Survey</v-btn>
+      </v-card>
+    </div>
     <!--END OF TIMELINE -->
   </v-app>
 </template>
@@ -52,8 +104,8 @@ export default {
       value: 0,
       fruits: 0,
       ticksLabels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-      dropdown_font: ["Arial", "Calibri", "Courier", "Verdana"],
-      items: ["Headache", "Anxiety", "Gastro Intestinal Pain"]
+      items: ["Headache", "Anxiety", "Gastro Intestinal Pain"],
+      time: "00:00:00"
     };
   }
 };
